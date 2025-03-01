d9850187479d5645f3810f84a5dcdd714ada64f4
Miraioff <miraik915@gmail.com>
2025-02-28 21:02:39 -0300

    Somando dois números

---
+++ Exercicios/Ex003.py
@@ -0,0 +1,3 @@
+n1 = int(input('\033[1;34mDigite um numero: \033[m'))
+n2 = int(input('\033[1;34mDigite outro numero: \033[m'))
+print(f'\033[1;34mA soma entre {n1} e {n2} e igual a \033[m \033[32m{n1+n2}\033[m')
